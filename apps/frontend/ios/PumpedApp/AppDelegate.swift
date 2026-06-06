be91c754dbab4e0ca69594f04f94242d12de3dc5
Dawitschi <dawitschi008@gmail.com>
2026-06-06 22:11:41 +0200

    Fix native app entry registration

--- apps/frontend/ios/PumpedApp/AppDelegate.swift
+++ apps/frontend/ios/PumpedApp/AppDelegate.swift
@@ -24,7 +24,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     window = UIWindow(frame: UIScreen.main.bounds)
 
     factory.startReactNative(
-      withModuleName: "PumpedApp",
+      withModuleName: "main",
       in: window,
       launchOptions: launchOptions
     )
